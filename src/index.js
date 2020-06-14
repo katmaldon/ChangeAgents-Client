0038cae1a0fd9392d7adc3dceb4eb59c48400f11
rwilt <rosie.wilt@gmail.com>
2020-06-14 10:42:36 -0400

    styling

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "300px";
+    document.getElementById("main").style.marginLeft = "300px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
