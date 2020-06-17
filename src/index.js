7b6f1a70e5851b0e9ecd975e60be4780bf6d7169
rwilt <rosie.wilt@gmail.com>
2020-06-17 17:05:32 -0400

    stuff

--- src/index.js
+++ src/index.js
@@ -68,12 +68,17 @@ function titleColor(){
   
 
   events.addEventListener('click', () =>{
-    document.body.setAttribute('class', 'inner');
+    let bg1 = document.querySelector('.bg1')
+    let bg2 = document.querySelector('.bg2')
+    let bg3 = document.querySelector('.bg3')
+      bg1.src = ""
+      bg2.src = ""
+      bg3.src = ""
     profileCards.innerHTML =""
     div.innerHTML = ""
    fetch('http://localhost:3000/events')
     .then(resp => resp.json())
-    .then(json => renderEvents(json))
+    .then(json => renderEvents(json));
 }
 );
 }
@@ -132,7 +137,12 @@ eventsClick();
 
   volunteer.addEventListener('click', () =>{
     profileCards.innerHTML =""
-    document.body.setAttribute('class', 'inner');
+    let bg1 = document.querySelector('.bg1')
+    let bg2 = document.querySelector('.bg2')
+    let bg3 = document.querySelector('.bg3')
+      bg1.src = ""
+      bg2.src = ""
+      bg3.src = ""
     div.innerHTML = ""
     profileCards.innerHTML = ""
    fetch('http://localhost:3000/resources')
@@ -146,35 +156,109 @@ volunteerClick();
 
 //PROFILE LOGIC *********************
 function renderProfile(json){
+  
   let greeting = ["Making a difference from ","Taking a stand in ", "Doing your part in ", "Changing the world from ", "Using your voice from "];
   const eventCards = document.querySelector('.eventCards');
   const profileCards = document.querySelector('.profileCards');
   const donationDiv = document.createElement('div');
   const myEvents = document.createElement('div');
   donationDiv.className = "donation";
+  
   myEvents.className = "my-events";
   eventCards.innerHTML = ""
   //figure out how to style the event card div w a 0 height for other pages,
   // or remove without issues.
   profileCards.innerHTML = `
   <h1>Hey, ${json.name}!</h1>
-  <p>${greeting[Math.round(Math.random() * 3)]} ${json.location}, New York</p>
-  
+  <h3>${greeting[Math.round(Math.random() * 3)]} ${json.location}, New York</h3>
  `
+//renderEventsDiv
+//renderDonationDiv
+myEvents.innerHTML = `
+<h1>Your Events</h1>
+${renderEvents()}
+`
+
+donationDiv.innerHTML = `
+${renderDonations()}
+`
 profileCards.append(donationDiv);
 profileCards.append(myEvents);
 }
 
+function renderEvents(json){
+
+return `
+<button>−</button>
+<p>Event1</p>
+<p>Event2</p>
+<button type="button">Add Event</button>
+`
+}
+
+function renderDonations(){
+  return `
+  <button>−</button>
+  <h1>Your Donations</h1>
+  <a href="https://www.gofundme.com/f/georgefloyd">George Floyd Memorial Fund</a> 
+  <br>
+  <a href="https://www.artsbusinesscollaborative.org/asp-products/the-okra-project-sponsored-project/">The Okra Project</a> 
+  <br>
+  <a href="https://www.classy.org/give/77372/#!/donation/checkout">Girls Who Code</a> 
+
+  `
+
+}
+
+
+
 function profileClick(){
 const profile = document.querySelector('.profile');
-
 profile.addEventListener('click', () => { 
-  document.body.setAttribute('class', 'inner');
+  let body = document.querySelector('body')
+  body.style.cssText = `
+  background-image: none;`
     fetch('http://localhost:3000/users/1')
     .then(resp => resp.json())
-    .then(json => renderProfile(json));
+    .then(json => renderProfile(json))
+    ;
 });
 
 }
 
 profileClick();
+
+function transition(){
+  const imgArr = ["https://assets.tvo.org/prod/s3fs-public/styles/full_width_1280/public/article-thumbnails/Black-Lives-Matter.jpg?EnUPuFwVloAYHIB.QK3q50EdkrQ9ovFU", "https://i.imgur.com/MI68j2w.jpg","https://api.time.com/wp-content/uploads/2019/03/march.jpeg?w=2000"]
+  let body = document.querySelector('body')
+  let bg1 = document.createElement('img')
+  let button = document.querySelector('#clicker')
+  let index = 0
+  bg1.className = bg1
+  button.addEventListener('click', (e)=>{
+    e.preventDefault()
+    console.log(e.target)
+    console.log("clicking")
+      if (index < imgArr.length -1 ){
+        e.preventDefault()
+      index++ 
+      body.style.cssText = `
+      background-image: url('${imgArr[index]}');
+      `
+      } 
+      else {
+        e.preventDefault()
+        index = 0
+        body.style.cssText = `
+        background-image: url('${imgArr[index]}');
+        `
+      }
+  })
+
+}
+
+
+
+transition()
+
+
